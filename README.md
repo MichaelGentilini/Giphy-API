12e041b0022ca80c57b60e73da94bb251342c414
dallasappraiser <46821219+dallasappraiser@users.noreply.github.com>
2019-04-13 17:03:48 -0500

    final version

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Giphy-API
+
+Press the Superhero Buttons and retrieve 10 images from the GIPHY API as well as information from the Wikipedia API and the Superhero.
+
+Users can also have the option of adding new superheros.
+
+## New things learned and used ##  
+
+- How to call APIs with GET and access various JSON data from the response
+- Used nth-of-type css property to change the button colors
+
+## Trivia Game can be found @: ##
+
+[https://github.com/dallasappraiser/Giphy-API](https://github.com/dallasappraiser/Giphy-API "Code")
+
+[https://dallasappraiser.github.io/Giphy-API/](https://dallasappraiser.github.io/Giphy-API/ "Available on Github")
+
+
+[https://dallasappraiser.github.io/Bootstrap-Portfolio/portfolio.html](https://dallasappraiser.github.io/Bootstrap-Portfolio/portfolio.html "Portfolio Page")
--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -12,23 +12,22 @@ $("document").ready(function () {
     "green arrow",
     "wolverine",
     "hulk",
-    "spider man",
+    "spider-man",
     "aquaman",
     "batman",
     "iron man",
     "daredevil",
-    "green lantern",
     "iron fist",
-    "cat woman"
+    "supergirl",
+    "marvel girl"
   ];
-  // var currentSuperHero = "";
-  var newSuperHero = [];
+
 
   var startBtn = function () {
-
     $(".empty-buttons").empty();
 
-    //! Dynamic superhero BUTTONS to start
+    // * Dynamic superhero BUTTONS to start
+
     for (let j = 0; j < superheroes.length; j++) {
       var index = j;
       // console.log(index);
@@ -50,7 +49,9 @@ $("document").ready(function () {
   $("#submitNewHero").on("click", function (event) {
     event.preventDefault();
 
-    var newHero = $("#newHero").val().trim();
+    var newHero = $("#newHero")
+      .val()
+      .trim();
     superheroes.push(newHero);
     startBtn();
   });
@@ -58,6 +59,8 @@ $("document").ready(function () {
   // * API call which creates the GIFs based on the currentSuperHero *
 
   function createGif() {
+
+    ;
     var queryURL =
       "https://api.giphy.com/v1/gifs/search?q=" +
       currentSuperHero +
@@ -68,16 +71,16 @@ $("document").ready(function () {
       url: queryURL,
       method: "GET"
     }).then(function (response) {
-      // newURL = queryURL
       var results = response.data;
+      console.log(response.data[0].images);
 
       var superDiv = $("<div>");
       superDiv.addClass("row");
       for (var i = 0; i < results.length; i++) {
-        // empty images container
+
         $(".images").empty();
 
-        // create a div with a column for the images
+        // ! create a div with a column for the images .. you can change the size of the images by manipulating the class
         var colHolder = $("<div>");
         colHolder.addClass("col-lg-3");
 
@@ -87,16 +90,106 @@ $("document").ready(function () {
         p.addClass("rating");
 
         var superImage = $("<img>");
+        superImage.addClass("heroImage");
         superImage.attr("src", results[i].images.fixed_width.url);
+        superImage.attr("data-still", results[i].images.fixed_width_still.url);
+        superImage.attr("data-animate", results[i].images.fixed_width.url);
+        superImage.attr("data-state", "animate");
 
-        // append image and rating to the column, div, and container
+
+        // * append image and rating to the column, div, and container
         colHolder.append(p, superImage);
         superDiv.append(colHolder);
         $(".images").prepend(superDiv);
+      }
 
+      // * This function allows for the gif animation to pause when pressed *
+      $(".heroImage").on("click", function () {
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
+    });
+  }
+
+  // * API call of Wikipedia using currentSuperHero *\
+
+  function getWiki() {
+
+    var wikiURL =
+      "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
+      currentSuperHero + "comic" +
+      "&format=json&callback=?";
+
+    $.ajax({
+        url: wikiURL,
+        method: "GET",
+        dataType: "json",
+        async: false
+      })
+      .done(function (data) {
+        var name = data[1][0];
+        var info = data[2][0];
+        var link = data[3][0];
+
+        $(".info").css("visibility", "visible");
+        $("#name").text(currentSuperHero);
+        $("#info").html(info);
+        $("#link").html("<a href =" + link + ">" + link + "</a>");
+      })
+      .fail(function () {
+        console.log("couldn't find anything");
+      });
+  }
+
+  // * API call of SuperHeroAPI *\
+
+  function createhero() {
+    var heroURL =
+      "http://www.superheroapi.com/api.php/517817718750245/search/" + currentSuperHero;
+
+    $.ajax({
+      url: heroURL,
+      method: "GET",
+    }).done(function (response) {
+      if (response.response == "success") {
+        var newResponse = response.results[0];
+
+        if ((currentSuperHero == "superman") || (currentSuperHero == "thor") || (currentSuperHero == "batman")) {
+          newResponse = response.results[1];
+        }
+
+        var publisher = response.results[0].biography.publisher;
+        var fullName = newResponse.biography["full-name"];
+        var aliases = newResponse.biography.aliases;
+        var base = newResponse.work.base;
+        var gender = newResponse.appearance.gender;
+        var height = newResponse.appearance.height;
+
+        $("#real-name").html("Real Name: " + fullName);
+        $("#publisher").html("Publisher: " + publisher);
+        $("#gender").html("Gender: " + gender);
+        $("#height").html("Height: " + height);
+        $("#base-city").html("Base City: " + base);
+        $("#aliases").html("Aliases: " + aliases);
+
+      } else if (response.response == "error") {
+        $("#real-name").html("<h4>We could not find this name in the superhero database. But, here is what we found in Wikipedia </h4>");
       }
     });
   }
+
+  // ! Create buttons to start and listen for clicks
   startBtn();
+  $(document).on("click", ".heroButton", getWiki);
   $(document).on("click", ".heroButton", createGif);
+  $(document).on("click", ".heroButton", createhero);
+
 });
